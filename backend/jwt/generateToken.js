9c74320ee2d907fa1e07ff9e16618fa8b809532a
Demo-github <pinky2023103@gmail.com>
2025-09-14 02:40:32 +0530

    Completed

---
+++ backend/jwt/generateToken.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+const createTokenAndSaveCookie = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_TOKEN, {
+    expiresIn: "10d",
+  });
+  res.cookie("jwt", token, {
+    httpOnly: true, // xss
+    //secure: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict", // csrf
+  });
+  return token;
+};
+export default createTokenAndSaveCookie;
